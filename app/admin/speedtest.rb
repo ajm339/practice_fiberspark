1474eb0e33690987d55f08f95706e01af3f782fd
Prelang Builder <builder@prelang.com>
2015-05-05 03:08:09 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Speedtest' to allow for create, update

--- app/admin/speedtest.rb
+++ app/admin/speedtest.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Speedtest do
+  permit_params :user_id, :provider, :ip_address
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
